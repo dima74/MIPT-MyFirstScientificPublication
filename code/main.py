331a439e9acadb6288b968fae220af66b9bb2d64
Дмитрий Мурзин <diraria@yandex.ru>
2018-03-14 18:58:30 +0300

    backup

--- code/main.py
+++ code/main.py
@@ -38,7 +38,7 @@ class ConvolutionLayer:
                         filter_input = input_padded[i:i + self.FILTER_SIZE, j:j + self.FILTER_SIZE, :]
                         output[filter_index][i][j] = (filter_weight * filter_input).sum() + filter_bias
             outputs.append(output)
-
+        outputs = np.array(outputs)
         cache = (inputs, weights, biases, outputs)
         return outputs, cache
 
@@ -70,7 +70,9 @@ class PoolingLayer:
         """
         outputs = []
         for input in inputs:
-            skimage.measure.block_reduce(input, (2, 2), np.max)
+            output = skimage.measure.block_reduce(input, (2, 2), np.max)
+            outputs.append(output)
+        outputs = np.array(outputs)
         cache = (inputs, outputs)
         return outputs, cache
 
@@ -90,23 +92,53 @@ class FCLayer:
             input_flatten = input.flatten()
             output = weights @ input_flatten
             outputs.append(output)
-
+        outputs = np.array(outputs)
         cache = (inputs, weights, biases, outputs)
         return outputs, cache
 
-    def backward(self, gradients_for_output, cache):
+    def backward(self, gradients_for_outputs, cache):
         """
-        :param gradients_for_output: (C)
-        :param cache: (inputs, outputs)
-        :return: ()
+        :param gradients_for_output: (N, C)
+        :param cache: (inputs, weights, biases, outputs)
+        :return: (gradients_for_inputs, gradients_for_parameters)
+            gradients_for_inputs: (N, height, width, depth)
+            gradients_for_parameters: (gradients_for_weights, gradients_for_biases)
+                gradients_for_weights: (C, height*width*depth)
+                gradients_for_biases: (C)
         """
-        pass
+
+        inputs, weights, biases, outputs = cache
+        N, height, width, depth = inputs.shape
+
+        gradients_for_inputs = []
+        gradients_for_weights = []
+        gradients_for_biases = np.zeros(NUMBER_CLASSES)
+        for input, output, gradients_for_output in zip(inputs, outputs, gradients_for_outputs):
+            input_flatten = input.flatten()
+            # gradients_for_input[i] = sum_c(d(L)/d(c) * d(c)/d(input[i]))
+            # d(L)/d(c) = gradients_for_output[c]
+            # d(c)/d(input[i]) = weights[c][i]
+            gradients_for_input = (weights * gradients_for_output.reshape((-1, 1))).sum(axis=0)
+            gradients_for_input = gradients_for_input.reshape((height, width, depth))
+            gradients_for_inputs.append(gradients_for_input)
+
+            # gradients_for_weights[c1][i] = sum_c2(d(L)/d(c2) * d(c2)/d(weight[c1][i])) = d(L)/d(c1) * d(c1)/d(weight[c1][i])
+            # c2 = sum_i(weight[c2][i] * input[i]) + b[c2]
+            gradients_for_weights += weights * gradients_for_output.reshape((-1, 1))
+
+            # gradients_for_biases[c1] = sum_c2(d(L)/d(c2) * d(c2)/d(c1)) = d(L)/d(c1) * 1
+            gradients_for_biases += gradients_for_output
+        gradients_for_weights /= N
+        gradients_for_biases /= N
+        return np.array(gradients_for_inputs)
 
 class SoftmaxLayer:
     def forward(self, inputs):
         """
         :param inputs: (N, C)
         :return: (outputs, cache)
+            outputs: (N, C)
+            cache: (inputs, outputs)
         """
 
         outputs = []
@@ -114,18 +146,19 @@ class SoftmaxLayer:
             input_exp = np.exp(input)
             output = input_exp / np.sum(input_exp)
             outputs.append(output)
+        outputs = np.array(outputs)
         cache = (inputs, outputs)
         return outputs, cache
 
-    def backward(self, gradients_for_output, cache):
+    def backward(self, gradients_for_outputs, cache):
         """
-        :param gradients_for_output: (C)
+        :param gradients_for_output: (N, C)
         :param cache: (inputs, outputs)
-        :return: (C)
+        :return: (N, C)
         """
         inputs, outputs = cache
         gradients_for_inputs = []
-        for input, output in zip(inputs, outputs):
+        for input, output, gradients_for_output in zip(inputs, outputs, gradients_for_outputs):
             input_exp = np.exp(input)
             # (d(output_i) / d(input_j)
             jacobian = -(input_exp.reshape((-1, 1)) @ input_exp.reshape((1, -1))) / (np.sum(input_exp) ** 2) + np.diag(output)
@@ -194,7 +227,7 @@ class Model:
         NUMBER_GRADIENT_ITERATIONS = 100
         GRADIENT_STEP = 1e-3
 
-        assert x_train.shape == (28, 28, 1)
+        assert x_train.shape[1:] == (28, 28, 1)
         for _ in range(NUMBER_GRADIENT_ITERATIONS):
             # forward pass
             outputs, caches = self.forward_pass(x_train)
@@ -209,8 +242,9 @@ class Model:
                 # производная ошибки по вероятностям классов
                 gradient_for_softmax_outputs = -(output_right / output)
                 gradients_for_softmax_outputs.append(gradient_for_softmax_outputs)
-            gradients_for_softmax_outputs = np.mean(gradients_for_softmax_outputs, axis=0)
-            assert gradients_for_softmax_outputs.shape == (NUMBER_CLASSES,)
+            # gradients_for_softmax_outputs = np.mean(gradients_for_softmax_outputs, axis=0)
+            gradients_for_softmax_outputs = np.array(gradients_for_softmax_outputs)
+            assert gradients_for_softmax_outputs.shape == (len(x_train), NUMBER_CLASSES)
 
             # backward pass
             gradients_for_outputs = gradients_for_softmax_outputs
