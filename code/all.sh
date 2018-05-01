d1549f4d53b1c659762df87c308476edc8ef3b18
Дмитрий Мурзин <diraria@yandex.ru>
2018-05-02 01:11:29 +0300

    backup

--- code/all.sh
+++ code/all.sh
@@ -1,18 +1,19 @@
 #!/bin/sh -e
 set -x
+ROOT="$( cd "$(dirname "$0")" ; pwd -P )"
 
-cd `dirname $0`
+cd $ROOT
 ./clear_all_results.sh
 
-cd `dirname $0`/1_data_discrete
+cd $ROOT/1_data_discrete
 python ./convert_to_directory_with_png_files.py
 python ./convert_to_single_pickle_file.py
 
-cd `dirname $0`/2_skeleton_creator
+cd $ROOT/2_skeleton_creator
 ./run.sh
 
-cd `dirname $0`/3_features_generator
+cd $ROOT/3_features_generator
 python ./features_generator.py
 
-cd `dirname $0`/4_merge_data_and_features
+cd $ROOT/4_merge_data_and_features
 python ./merge_data_and_features.py
